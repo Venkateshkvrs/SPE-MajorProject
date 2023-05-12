e66ec0d72228d005285587c1b6acf2e0611989b5
BOPPANA VENKATESH <boppanavenkatesh97@gmail.com>
2023-05-12 22:08:16 +0530

    Added Unit Test

--- backend/src/test/java/com/example/ebillpay/EBillPayApplicationTests.java
+++ backend/src/test/java/com/example/ebillpay/EBillPayApplicationTests.java
@@ -1,44 +1,78 @@
-//package com.example.ebillpay;
-//
-//import com.example.ebillpay.dto.OfficerDTO;
-//import com.example.ebillpay.entity.Officer;
-//import com.example.ebillpay.repository.BillsRepository;
-//import com.example.ebillpay.repository.ConnectionRepository;
-//import com.example.ebillpay.repository.LoginRepository;
-//import com.example.ebillpay.repository.OfficerRepository;
-//import com.example.ebillpay.service.AdminServices;
-//import com.example.ebillpay.service.AdminServicesImpl;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest
-//class EBillPayApplicationTests {
-//
-//    @Mock
-//    private OfficerRepository officerRepository;
-//
-//    private AdminServices adminServices;
-//
-//
-//    @Mock
-//    private LoginRepository loginRepository;
-//    @Mock
-//    private ConnectionRepository connectionRepository;
-//    @Mock
-//    private BillsRepository billsRepository;
-//
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.openMocks(this);
-//        adminServices = new AdminServicesImpl(officerRepository,loginRepository,connectionRepository,billsRepository);
-//    }
-//
+package com.example.ebillpay;
+
+import com.example.ebillpay.dto.CustomerDTO;
+import com.example.ebillpay.dto.OfficerDTO;
+import com.example.ebillpay.entity.Customer;
+import com.example.ebillpay.entity.Officer;
+import com.example.ebillpay.repository.BillsRepository;
+import com.example.ebillpay.repository.ConnectionRepository;
+import com.example.ebillpay.repository.LoginRepository;
+import com.example.ebillpay.repository.OfficerRepository;
+import com.example.ebillpay.service.AdminServices;
+import com.example.ebillpay.service.AdminServicesImpl;
+import com.example.ebillpay.service.CustomerService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class EBillPayApplicationTests {
+
+    @Mock
+    private OfficerRepository officerRepository;
+
+    private AdminServices adminServices;
+
+
+    @Mock
+    private LoginRepository loginRepository;
+    @Mock
+    private ConnectionRepository connectionRepository;
+    @Mock
+    private BillsRepository billsRepository;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        adminServices = new AdminServicesImpl(officerRepository,loginRepository,connectionRepository,billsRepository);
+    }
+
+    @Test
+    public void testLoginCustomer() {
+        String userName = "vaishu";
+        String password = "vaishu";
+
+        CustomerService customerService = null;
+
+        Customer mockCustomer = new Customer();
+        mockCustomer.setAddress("123 Main St");
+        mockCustomer.setEmail("vaishnavi@gmail.com");
+        mockCustomer.setFirstName("Vashnavi");
+        mockCustomer.setLastName("Gangarde");
+        mockCustomer.setMobileNumber("9865321470");
+        customerService.addCustomer(mockCustomer);
+
+        CustomerDTO expected = new CustomerDTO();
+        expected.setCustomerId(1);
+        expected.setAddress("123 Main St");
+        expected.setEmail("vaishnavi@example.com");
+        expected.setFirstName("Vashnavi");
+        expected.setLastName("Gangarde");
+        expected.setMobileNumber("9865321470");
+
+        CustomerDTO actual = customerService.loginCustomer(userName, password);
+
+        expected.setCustomerId(actual.getCustomerId());
+
+        assertEquals(expected, actual);
+    }
+
 //    @Test
 //    public void testLoginOfficer_ValidCredentials_ReturnsOfficerDTO() {
 //        // Arrange
@@ -70,4 +104,4 @@
 //    }
 //
 //
-//}
+}
