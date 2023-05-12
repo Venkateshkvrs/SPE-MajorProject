a40660324acf519bac1500aa762afd413546b21a
BOPPANA VENKATESH <boppanavenkatesh97@gmail.com>
2023-05-13 00:03:59 +0530

    updated Unit Tests

--- backend/src/test/java/com/example/ebillpay/EBillPayApplicationTests.java
+++ backend/src/test/java/com/example/ebillpay/EBillPayApplicationTests.java
@@ -1,189 +1,49 @@
-//package com.example.ebillpay;
-//
-//import com.example.ebillpay.dto.CustomerDTO;
-//import com.example.ebillpay.entity.Customer;
-//import com.example.ebillpay.repository.LoginRepository;
-//import com.example.ebillpay.service.CustomerService;
-//import com.example.ebillpay.service.CustomerServiceImpl;
-//import org.aspectj.lang.annotation.Before;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.data.domain.Example;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.domain.Sort;
-//import org.springframework.data.repository.query.FluentQuery;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//
-//import java.util.List;
-//import java.util.Optional;
-//import java.util.function.Function;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.when;
-//
-//
-//@SpringBootTest
-//class EBillPayApplicationTests {
-//
-//    @Mock
-//    private LoginRepository loginRepo;
-//
-//    @InjectMocks
-//    private CustomerServiceImpl customerService;
-//
-//    @Before("")
-//    public void setup() {
-//        // Setup any mock data or behavior here
-//    }
-//
-//    @Test
-//    public void testLoginCustomer() {
-//        String userName = "vaishu";
-//        String password = "vaishu";
-//
-////        CustomerService customerService = null;
-//
-//        Customer mockCustomer = new Customer();
-//        mockCustomer.setAddress("123 Main St");
-//        mockCustomer.setEmail("vaishnavi@gmail.com");
-//        mockCustomer.setFirstName("Vashnavi");
-//        mockCustomer.setLastName("Gangarde");
-//        mockCustomer.setMobileNumber("9865321470");
-//        mockCustomer.setPassword("vaishu");
-//        mockCustomer.setUserName("vaishu");
-//
-//        System.out.println(mockCustomer.getEmail());
-//        customerService.addCustomer(mockCustomer);
-//
-//        CustomerDTO expected = new CustomerDTO();
-////        expected.setCustomerId(1);
-//        expected.setAddress("123 Main St");
-//        expected.setEmail("vaishnavi@example.com");
-//        expected.setFirstName("Vashnavi");
-//        expected.setLastName("Gangarde");
-//        expected.setMobileNumber("9865321470");
-//
-//        CustomerDTO actual = customerService.loginCustomer(userName, password);
-//
-//        if(actual!=null)
-//            expected.setCustomerId(actual.getCustomerId());
-//        if(actual != null)
-//            assertEquals(expected, actual);
-//        else
-//            assertNotEquals(expected,actual);
-//    }
-//
-//}
-//package com.example.ebillpay;
-//
-//import com.example.ebillpay.dto.CustomerDTO;
-//import com.example.ebillpay.dto.OfficerDTO;
-//import com.example.ebillpay.entity.Customer;
-//import com.example.ebillpay.entity.Officer;
-//import com.example.ebillpay.repository.BillsRepository;
-//import com.example.ebillpay.repository.ConnectionRepository;
-//import com.example.ebillpay.repository.LoginRepository;
-//import com.example.ebillpay.repository.OfficerRepository;
-//import com.example.ebillpay.service.AdminServices;
-//import com.example.ebillpay.service.AdminServicesImpl;
-//import com.example.ebillpay.service.CustomerService;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest
-//class EBillPayApplicationTests {
-//
-//    @Mock
-//    private OfficerRepository officerRepository;
-//
-//    private AdminServices adminServices;
-//
-//
-//    @Mock
-//    private LoginRepository loginRepository;
-//    @Mock
-//    private ConnectionRepository connectionRepository;
-//    @Mock
-//    private BillsRepository billsRepository;
-//
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.openMocks(this);
-//        adminServices = new AdminServicesImpl(officerRepository,loginRepository,connectionRepository,billsRepository);
-//    }
-//
-//    @Test
-//    public void testLoginCustomer() {
-//        String userName = "vaishu";
-//        String password = "vaishu";
-//
-//        CustomerService customerService = null;
-//
-//        Customer mockCustomer = new Customer();
-//        mockCustomer.setAddress("123 Main St");
-//        mockCustomer.setEmail("vaishnavi@gmail.com");
-//        mockCustomer.setFirstName("Vashnavi");
-//        mockCustomer.setLastName("Gangarde");
-//        mockCustomer.setMobileNumber("9865321470");
-//        customerService.addCustomer(mockCustomer);
-//
-//        CustomerDTO expected = new CustomerDTO();
-//        expected.setCustomerId(1);
-//        expected.setAddress("123 Main St");
-//        expected.setEmail("vaishnavi@example.com");
-//        expected.setFirstName("Vashnavi");
-//        expected.setLastName("Gangarde");
-//        expected.setMobileNumber("9865321470");
-//
-//        CustomerDTO actual = customerService.loginCustomer(userName, password);
-//        if(actual != null)
-//            expected.setCustomerId(actual.getCustomerId());
-//
-//        assertEquals(expected, actual);
-//    }
-//
-////    @Test
-////    public void testLoginOfficer_ValidCredentials_ReturnsOfficerDTO() {
-////        // Arrange
-////        String userName = "admin";
-////        String password = "password";
-////        Officer officer = new Officer();
-////        officer.setMobileNumber("1234567890");
-////        officer.setArea("Example Area");
-////        officer.setOfficerName("John Doe");
-////        officer.setEmail("john@example.com");
-////        officer.setOfficerId(1);
-////        officer.setRole("admin");
-////        officer.setUserName("admin");
-////        officer.setPassword("password");
-////
-////        when(officerRepository.findOneByUserNameAndPassword(userName, password)).thenReturn(officer);
-////
-////        // Act
-////        OfficerDTO result = adminServices.loginOfficer(userName, password);
-////
-////        // Assert
-////        Assertions.assertNotNull(result);
-////        Assertions.assertEquals(officer.getMobileNumber(), result.getMobileNumber());
-////        Assertions.assertEquals(officer.getArea(), result.getArea());
-////        Assertions.assertEquals(officer.getOfficerName(), result.getOfficerName());
-////        Assertions.assertEquals(officer.getEmail(), result.getEmail());
-////        Assertions.assertEquals(officer.getOfficerId(), result.getOfficerId());
-////        Assertions.assertEquals(officer.getRole(), result.getRole());
-////    }
-////
-////
-//}
+package com.example.ebillpay;
+
+import com.example.ebillpay.entity.Customer;
+import com.example.ebillpay.repository.LoginRepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+@SpringBootTest
+class EBillPayApplicationTests {
+
+    @Before("")
+    public void setup() {
+        // Setup any mock data or behavior here
+    }
+
+    @Test
+    public void testLoginCustomer() {
+        String userName = "vaishu";
+        String password = "vaishu";
+
+        Customer mockCustomer = new Customer();
+        mockCustomer.setAddress("123 Main St");
+        mockCustomer.setEmail("vaishnavi@gmail.com");
+        mockCustomer.setFirstName("Vashnavi");
+        mockCustomer.setLastName("Gangarde");
+        mockCustomer.setMobileNumber("9865321470");
+        mockCustomer.setPassword("vaishu");
+        mockCustomer.setUserName("vaishu");
+
+        LoginRepository mockRepo = mock(LoginRepository.class);
+        when(mockRepo.findOneByUserNameAndPassword("vaishu", "vaishu")).thenReturn(mockCustomer);
+
+        Customer result = mockRepo.findOneByUserNameAndPassword("vaishu","vaishu");
+        assertNotNull(result);
+        assertEquals(mockCustomer.getCustomerId(), result.getCustomerId());
+        assertEquals(mockCustomer.getFirstName(), result.getFirstName());
+        assertEquals(mockCustomer.getLastName(), result.getLastName());
+        assertEquals(mockCustomer.getEmail(), result.getEmail());
+        assertEquals(mockCustomer.getMobileNumber(), result.getMobileNumber());
+        assertEquals(mockCustomer.getAddress(), result.getAddress());
+    }
+
+}
